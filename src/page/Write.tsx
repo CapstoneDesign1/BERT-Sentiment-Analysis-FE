e75c204ad7d016d2172566947eb25a21e584043c
심의석 <simsuli9@naver.com>
2021-11-23 02:45:46 +0900

    FEAT : #4 Implement Register User Diary

--- src/page/Write.tsx
+++ src/page/Write.tsx
@@ -4,6 +4,7 @@ import React, {FormEvent, useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import {getQuestions, IQuestionDto} from "../apis/Question";
 import { Cookies } from 'react-cookie';
+import {registerDiary} from "../apis/Diary";
 
 const Write = () : JSX.Element => {
 
@@ -17,6 +18,7 @@ const Write = () : JSX.Element => {
     });
     const [qNum, setQNum] = useState(0);
     const [qList, setQList] = useState<IQuestionDto[]>([]);
+    const [questionIdList, setQuestionIdList] = useState('');
     const cookies = new Cookies();
     const navigator = useNavigate();
 
@@ -29,7 +31,10 @@ const Write = () : JSX.Element => {
     }, []);
 
     useEffect(() => {
-        console.log(qList);
+        qList.map((item, index) => (
+            index === 0 ? setQuestionIdList((questionIdList) => questionIdList.concat(item.id.toString()))
+                        : setQuestionIdList((questionIdList) => questionIdList.concat("#" + item.id.toString()))
+        ));
     }, [qList]);
 
     const handleGetQuestion = async () => {
@@ -49,8 +54,10 @@ const Write = () : JSX.Element => {
         setForm(nextForm);
     }
 
-    const handleKeyPress = (e : React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const handleKeyPress = async (e : React.KeyboardEvent<HTMLTextAreaElement>) => {
         if (e.key === 'Enter' && !e.shiftKey) {
+            if (qNum === 4)
+                return ;
             setQNum((qNum) => qNum + 1);
         }
     }
@@ -59,15 +66,25 @@ const Write = () : JSX.Element => {
         setQNum((qNum) => qNum - 1);
     }
 
-    const handleOnSubmit = (e : FormEvent) => {
-        e.preventDefault();
+    const onRegister = async () => {
         if (answer1 && answer2 && answer3 && answer4 && answer5) {
-            alert("작성을 완료했습니다.");
-            navigator('/diary');
+            const userId = cookies.get('userId');
+            try {
+                await registerDiary({userId, answer1, answer2, answer3, answer4, answer5, questionIdList});
+                alert("작성을 완료했습니다.");
+                navigator('/diary');
+            } catch (e) {
+                alert("잠시후 다시 시도해주세요.");
+            }
         }
         else {
             alert("모든 질문에 답해주세요.");
         }
+    }
+
+    const handleOnSubmit = async (e : FormEvent) => {
+        e.preventDefault();
+        await onRegister();
     }
 
     const {answer1, answer2, answer3, answer4, answer5} = form;
@@ -78,7 +95,7 @@ const Write = () : JSX.Element => {
             <div className={style.box}>
                 <div className={style.form_container}>
                     <span className={style.form_title}>당신의 속마음을 들려주세요.</span>
-                    {qList.length && <form className={style.form_box} onSubmit={handleOnSubmit}>
+                    {qList.length ? <form className={style.form_box} onSubmit={handleOnSubmit}>
                         {qNum === 0 &&
                         <div className={qNum === 0 ? style.input_container : style.input_container_close}>
                             <span className={style.question_title}>{title[qNum]}</span>
@@ -146,12 +163,12 @@ const Write = () : JSX.Element => {
                                 name={"answer5"}
                             />
                             <div className={style.button_box}>
-                            <button className={style.previous_button} onClick={handleOnClick}>이전</button>
+                                <button className={style.previous_button} onClick={handleOnClick}>이전</button>
                                 <button className={style.register_button} type={"submit"}>완료</button>
                             </div>
                         </div>
                         }
-                    </form>}
+                    </form> : null}
                 </div>
             </div>
         </div>
